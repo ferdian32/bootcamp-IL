3a665cb32176584f526c1a2371658533a28eccef
FierliFerdiansyah1 <fierliferdiansyah0@gmail.com>
2025-02-10 18:33:38 +0700

    first commit

---
+++ latihan.js
@@ -0,0 +1,39 @@
+// let a = 10;
+
+// if (a > 10){
+//     console.log('Time to Sleep');
+// }else{
+//     console.log('Time to Cook');
+// }
+
+var date = 3;
+var month 
+
+switch (date) {
+    case 1:
+        month = "January";
+        console.log(month);
+        break;
+    case 2:
+        month = "February";
+        console.log(month);
+        break;
+    case 3:
+        month = "March";
+        console.log(month);
+        break;
+    case 4:
+        month = "April";
+        console.log(month);
+        break;
+    default:
+        month = "Nothing";
+        console.log(month);
+        break;
+}
+
+for (let i = 0; i <=10; i++) {
+    console.log(i);
+    
+}
+
